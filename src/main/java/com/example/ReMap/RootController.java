6e6fe1933abdec4dd7087293c39b264343f86c7a
KimChaeWon12 <kcw030220@naver.com>
2025-07-13 04:59:15 +0900

    update: CI/CD 파이프라인 수정

---
+++ src/main/java/com/example/ReMap/RootController.java
@@ -0,0 +1,10 @@
+package com.example.ReMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/health")
+    public String healthCheck() { return "BodyCheck health check!"; }
+}
