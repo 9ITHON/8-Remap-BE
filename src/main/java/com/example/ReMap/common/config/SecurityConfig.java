5177e61164278c1ce4652b585ffd7391ce5c3452
KimChaeWon12 <kcw030220@naver.com>
2025-07-12 20:56:22 +0900

    add: securityconfig 추가

---
+++ src/main/java/com/example/ReMap/common/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.ReMap.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(registry -> registry.requestMatchers("**").permitAll())
+                .authorizeHttpRequests(registry -> registry
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    //패스워드 암호화 관련 매소드
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
